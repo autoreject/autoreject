9bcd7e935818cafb338384bc07e13431a83c2469
Mainak Jas <mainakjas@gmail.com>
2016-09-25 23:50:57 +0200

    preload = False + other updates

--- examples/plot_visualize_bad_epochs.py
+++ examples/plot_visualize_bad_epochs.py
@@ -11,48 +11,56 @@ visualize the bad sensors in each trial
 # License: BSD (3-clause)
 
 ###############################################################################
-# First, we download the data from openfMRI. We will download the tarfile,
+# First, we download the data from OpenfMRI. We will download the tarfile,
 # extract the necessary files and delete the tar from the disk
+
 ###############################################################################
 
 import os
-import os.path as op
-import mne
 import tarfile
 
 import autoreject
-from autoreject import LocalAutoRejectCV, plot_epochs
 from autoreject.utils import fetch_file
 
-subject_id = 16  # Openfmri format
-tmin, tmax = -0.2, 0.8
-events_id = {'famous/first': 5, 'famous/immediate': 6, 'famous/long': 7}
+subject_id = 16  # OpenfMRI format of subject numbering
 
 src_url = ('http://openfmri.s3.amazonaws.com/tarballs/'
            'ds117_R0.1.1_sub016_raw.tgz')
-
 subject = "sub%03d" % subject_id
-print("processing subject: %s" % subject)
 
-base_path = op.join(op.dirname(autoreject.__file__), '..', 'examples')
-target = op.join(base_path, 'ds117_R0.1.1_sub016_raw.tgz')
-if not op.exists(op.join(base_path, 'ds117')):
-    if not op.exists(target):
+print("processing subject: %s" % subject)
+base_path = os.path.join(os.path.dirname(autoreject.__file__), '..', 'examples')
+target = os.path.join(base_path, 'ds117_R0.1.1_sub016_raw.tgz')
+if not os.path.exists(os.path.join(base_path, 'ds117')):
+    if not os.path.exists(target):
         fetch_file(src_url, target)
     tf = tarfile.open(target)
     print('Extracting files. This may take a while ...')
     tf.extractall(path=base_path, members=tf.getmembers()[-25:-9:3])
     os.remove(target)
 
+###############################################################################
+# We will create epochs with data starting 200 ms before trigger onset
+# and continuing up to 800 ms after that. The data contains visual stimuli for
+# famous faces, unfamiliar faces, as well as scrambled faces.
+
+###############################################################################
+
+tmin, tmax = -0.2, 0.8
+events_id = {'famous/first': 5, 'famous/immediate': 6, 'famous/long': 7}
+
+###############################################################################
+# Let us now load all the epochs into memory and concatenate them
+
+###############################################################################
+import mne
+
 epochs = list()
 for run in range(1, 7):
-    run_fname = op.join(base_path, 'ds117',
-                        'sub%03d' % subject_id, 'MEG',
-                        'run_%02d_raw.fif' % run)
+    run_fname = os.path.join(base_path, 'ds117', 'sub%03d' % subject_id, 'MEG',
+                             'run_%02d_raw.fif' % run)
     raw = mne.io.Raw(run_fname, preload=True, add_eeg_ref=False)
-    mne.io.set_eeg_reference(raw, [])
-    # less memory + computation
-    raw.pick_types(eeg=True, meg=False, stim=True)
+    raw.pick_types(eeg=True, meg=False, stim=True) # less memory + computation
     raw.filter(1, 40, n_jobs=1, verbose='INFO')
 
     raw.set_channel_types({'EEG061': 'eog', 'EEG062': 'eog',
@@ -67,19 +75,31 @@ for run in range(1, 7):
                              consecutive='increasing',
                              min_duration=0.003, verbose=True)
     # Read epochs
-    epoch = mne.Epochs(raw, events, events_id, tmin, tmax, proj=False,
-                       add_eeg_ref=False, picks=picks, baseline=None,
-                       preload=True, reject=None, decim=4)
+    epoch = mne.Epochs(raw, events, events_id, tmin, tmax, proj=True,
+                       add_eeg_ref=True, picks=picks, baseline=None,
+                       preload=False, reject=None, decim=4)
     epochs.append(epoch)
-    mne.io.set_eeg_reference(epoch)
-    if run > 1:
-        epoch.info['dev_head_t'] = epochs[0].info['dev_head_t']
+
+    # Same `dev_head_t` for all runs so that we can concatenate them.
+    epoch.info['dev_head_t'] = epochs[0].info['dev_head_t']
 epochs = mne.epochs.concatenate_epochs(epochs)
 
+###############################################################################
+# Now, we apply autoreject
+
+###############################################################################
+from autoreject import LocalAutoRejectCV
 this_epoch = epochs['famous']
 ar = LocalAutoRejectCV()
 epochs_ar = ar.fit_transform(this_epoch.copy())
 
+###############################################################################
+# ... and visualize the bad epochs and sensors. Bad sensors which have been
+# interpolated are in blue. Bad sensors which are not interpolated are in red.
+# Bad trials are also in red.
+
+###############################################################################
+from autoreject import plot_epochs
 plot_epochs(this_epoch, bad_epochs_idx=ar.bad_epochs_idx,
             fix_log=ar.fix_log, scalings=dict(eeg=40e-6),
             title='')
