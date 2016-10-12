b8f51139809bb29e6274857c5450ebdc39a60726
Mainak Jas <mainak.jas@telecom-paristech.fr>
2016-10-12 15:58:44 +0200

    Improve test coverage

--- autoreject/tests/test_autoreject.py
+++ autoreject/tests/test_autoreject.py
@@ -8,7 +8,8 @@ import mne
 from mne.datasets import sample
 from mne import io
 
-from autoreject import GlobalAutoReject, LocalAutoReject, validation_curve
+from autoreject import (GlobalAutoReject, LocalAutoReject, compute_thresholds,
+                        validation_curve)
 from autoreject.utils import clean_by_interp
 from autoreject.viz import plot_epochs
 
@@ -19,8 +20,8 @@ matplotlib.use('Agg')
 
 data_path = sample.data_path()
 raw_fname = data_path + '/MEG/sample/sample_audvis_filt-0-40_raw.fif'
-raw = io.read_raw_fif(raw_fname, preload=False)
-raw.crop(0, 10)
+raw = io.read_raw_fif(raw_fname, add_eeg_ref=False, preload=False)
+raw.crop(0, 15)
 raw.info['projs'] = list()
 
 
@@ -31,29 +32,40 @@ def test_autoreject():
     tmin, tmax = -0.2, 0.5
     events = mne.find_events(raw)
 
-    include = []
-    picks = mne.pick_types(raw.info, meg=False, eeg=True, stim=False,
-                           eog=False, include=include, exclude='bads')
+    include = [u'EEG %03d' % i for i in range(1, 15)]
+    picks = mne.pick_types(raw.info, meg=False, eeg=False, stim=False,
+                           eog=False, include=include, exclude=[])
     epochs = mne.Epochs(raw, events, event_id, tmin, tmax,
-                        picks=picks, baseline=(None, 0),
-                        reject=None, verbose=False)
+                        picks=picks, baseline=(None, 0), decim=8,
+                        reject=None, add_eeg_ref=False)
 
-    ar = GlobalAutoReject()
     X = epochs.get_data()
     n_epochs, n_channels, n_times = X.shape
     X = X.reshape(n_epochs, -1)
 
+    ar = GlobalAutoReject()
     assert_raises(ValueError, ar.fit, X)
-    assert_raises(ValueError, ar.fit, X, n_channels)
+    ar = GlobalAutoReject(n_channels=n_channels)
+    assert_raises(ValueError, ar.fit, X)
+    ar = GlobalAutoReject(n_times=n_times)
+    assert_raises(ValueError, ar.fit, X)
+    ar = GlobalAutoReject(n_channels=n_channels, n_times=n_times,
+                          thresh=40e-6)
+    ar.fit(X)
 
     param_name = 'thresh'
-    param_range = np.linspace(40e-6, 200e-6, 30)
+    param_range = np.linspace(40e-6, 200e-6, 10)
     assert_raises(ValueError, validation_curve, ar, X, None,
                   param_name, param_range)
 
     ar = LocalAutoReject()
     assert_raises(NotImplementedError, validation_curve, ar, epochs, None,
                   param_name, param_range)
+
+    epochs.load_data()
+    assert_raises(ValueError, compute_thresholds, epochs, 'dfdfdf')
+    for method in ['random_search', 'bayesian_optimization']:
+        compute_thresholds(epochs, method=method)
 
 
 def test_utils():
