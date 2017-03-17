9e780e5354dff5511c16ddadf02a0ab3be83cdfc
Mainak Jas <mainakjas@gmail.com>
2017-03-17 01:05:14 +0100

    DOC: caching builds on circle

--- examples/plot_visualize_bad_epochs.py
+++ examples/plot_visualize_bad_epochs.py
@@ -91,7 +91,7 @@ from functools import partial  # noqa
 this_epoch = epochs['famous']
 thresh_func = partial(compute_thresholds, random_state=42)
 
-ar = LocalAutoRejectCV(thresh_func=thresh_func, verbose='progressbar')
+ar = LocalAutoRejectCV(thresh_func=thresh_func, verbose='tqdm')
 epochs_ar = ar.fit_transform(this_epoch.copy())
 
 ###############################################################################
