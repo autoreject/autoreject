aa18a5e4301e7450e9a0f3d9e21ec26550983b5b
dengemann <denis.engemann@gmail.com>
2018-05-20 11:49:00 +0200

    revert debug stuff

--- examples/plot_visualize_bad_epochs.py
+++ examples/plot_visualize_bad_epochs.py
@@ -108,7 +108,7 @@ thresh_func = partial(compute_thresholds, random_state=42, n_jobs=1)
 # representative subsample of the data.
 
 
-ar = LocalAutoRejectCV(thresh_func=thresh_func, picks=picks)
+ar = LocalAutoRejectCV(thresh_func=thresh_func, picks=picks, verbose='tqdm')
 
 epochs_ar, reject_log = ar.fit_transform(this_epoch, return_log=True)
 
