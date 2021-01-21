deb58780040562ef469714a3521d30c42e18ee0b
msseibel <marc-steffen.seibel@tu-ilmenau.de>
2021-01-21 20:45:00 +0100

    enable user to set custom cv object for get_rejection_threshold

--- autoreject/autoreject.py
+++ autoreject/autoreject.py
@@ -260,7 +260,7 @@ def get_rejection_threshold(epochs, decim=1, random_state=None,
         best_thresh, _ = bayes_opt(func, initial_x,
                                    all_threshes,
                                    expected_improvement,
-                                   max_iter=15, debug=False,
+                                   max_iter=10, debug=False,
                                    random_state=random_state)
         reject[ch_type] = best_thresh
 
@@ -722,10 +722,7 @@ class _AutoReject(BaseAutoReject):
 
 def _interpolate_bad_epochs(
         epochs, interp_channels, picks, dots=None, verbose='progressbar'):
-    """Actually do the interpolation.
-    Apply interpolation to each epoch separately.
-    param: interp_channels is list of channel names
-    """
+    """Actually do the interpolation."""
     assert len(epochs) == len(interp_channels)
     pos = 2
 
@@ -952,7 +949,7 @@ class AutoReject(object):
         _check_data(epochs, picks=self.picks_, verbose=self.verbose)
         self.cv_ = self.cv
         if isinstance(self.cv_, int):
-            self.cv_ = KFold(n_splits=self.cv_,shuffle=True)
+            self.cv_ = KFold(n_splits=self.cv_)
 
         # XXX : maybe use an mne function in pick.py ?
         picks_by_type = _get_picks_by_type(info=epochs.info, picks=self.picks_)
