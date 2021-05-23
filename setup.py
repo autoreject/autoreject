88769f86e9feba1fec8bf7e232bfdba28c334b24
Richard Höchenberger <richard.hoechenberger@gmail.com>
2021-05-23 23:25:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ if __name__ == "__main__":
                   'numpydoc',
                   'cython',
                   'pillow',
-                  'openneuro-py >= 2021.5'
+                  'openneuro-py >= 2021.7'
               ]
           },
           packages=find_packages(),
