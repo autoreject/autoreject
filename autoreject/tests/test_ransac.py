efedada873c4a2223665d67a5753afa0a3c5f7a3
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-08-08 14:24:10 +0200

    typo corrected.

--- autoreject/tests/test_ransac.py
+++ autoreject/tests/test_ransac.py
@@ -39,7 +39,7 @@ def test_ransac():
     # Pass string instead of array of idx
     picks = 'eeg'
     ransac = Ransac(picks=picks)
-    epochs = ransac.fit(epochs[:2])
+    epochs_clean = ransac.fit(epochs[:2])
     assert len(epochs_clean[:2]) == len(epochs[:2])
     expected = mne.pick_types(epochs.info, meg=False, eeg=True, stim=False,
                               eog=False, exclude='bads')
