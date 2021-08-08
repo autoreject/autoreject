3064c9961da7aff694fe1bddede2d9742e2611db
Mathieu Scheltienne <73893616+mscheltienne@users.noreply.github.com>
2021-08-08 11:23:06 +0200

    Removed random_state from test.
    Co-authored-by: Mainak Jas <jasmainak@users.noreply.github.com>

--- autoreject/tests/test_ransac.py
+++ autoreject/tests/test_ransac.py
@@ -38,7 +38,7 @@ def test_ransac():
 
     # Pass string instead of array of idx
     picks = 'eeg'
-    ransac = Ransac(picks=picks, random_state=np.random.RandomState(42))
+    ransac = Ransac(picks=picks)
     epochs = ransac.fit(epochs[:2])
     assert len(epochs_clean[:2]) == len(epochs[:2])
     expected = mne.pick_types(epochs.info, meg=False, eeg=True, stim=False,
