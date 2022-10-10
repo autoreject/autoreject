0167c45f8d860cf2494097288a03a7d0092695ab
Eric Larson <larson.eric.d@gmail.com>
2022-10-10 13:19:45 -0400

    FIX: Try another

--- autoreject/conftest.py
+++ autoreject/conftest.py
@@ -4,3 +4,10 @@
 
 
 pytest_plugins = ['mne.conftest']
+
+
+def pytest_configure(config):
+    # Fixtures
+    for fixture in ('matplotlib_config', 'close_all', 'check_verbose',
+                    'qt_config', 'protect_config'):
+        config.addinivalue_line('usefixtures', fixture)
