45470a02e7bde751634dc3bf5cd0ef987c88c181
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2023-05-27 21:26:17 +0200

    fix verswitch button

--- doc/conf.py
+++ doc/conf.py
@@ -108,9 +108,10 @@ html_theme_options = {
     'use_edit_page_button': False,
     'navigation_with_keys': False,
     'show_toc_level': 1,
+    "navbar_end": ["theme-switcher", "version-switcher", "navbar-icon-links"],
     "switcher": {
         # "json_url": "https://mne.tools/dev/_static/versions.json",
-        "json_url": "https://github.com/sappelhoff/autoreject/blob/pydata/sphinx/doc/_static/versions.json",
+        "json_url": "https://raw.githubusercontent.com/sappelhoff/autoreject/pydata/sphinx/doc/_static/versions.json",  # noqa: E501
         "version_match": switcher_version_match,
     },
 }
