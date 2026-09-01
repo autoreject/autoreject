ba83e6ff3169e41621009aacc45dbeb79f0f29f5
Eric Larson <larson.eric.d@gmail.com>
2026-09-01 14:03:06 -0400

    FIX: Parallel

--- doc/conf.py
+++ doc/conf.py
@@ -3,6 +3,8 @@ import os
 import sys
 from datetime import date
 
+from sphinx.config import is_serializable
+
 import autoreject
 
 
@@ -147,4 +149,8 @@ sphinx_gallery_conf = {
     'backreferences_dir': 'generated',
     'examples_dirs': '../examples',
     'gallery_dirs': 'auto_examples',
+    # True means "use the -j value that sphinx-build was given" (see doc/Makefile)
+    'parallel': True,
 }
+# parallel example execution pickles the config, so make sure it can be
+assert is_serializable(sphinx_gallery_conf)
